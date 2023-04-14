795bd35e9350b738150f5e8616d0970814cd702b
Thai Pham <arknightgamersia03@gmail.com>
2023-04-13 19:05:16 -0700

    add some stuff

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
-'use strict'
+'use strict';
+const colorBtn = document.getElementById('colorBtn');
+const rainbowBtn = document.getElementById('rainbowBtn');
+
+const eraserBtn = document.getElementById('eraserBtn');
+
+const clearBtn = document.getElementById('clearBtn');
+
+const btnClickedAffect = function () {
+  colorBtn.addEventListener('click', () => {
+    colorBtn.classList.add('active');
+    rainbowBtn.classList.remove('active');
+    eraserBtn.classList.remove('active');
+  });
+  rainbowBtn.addEventListener('click', () => {
+    rainbowBtn.classList.add('active');
+    colorBtn.classList.remove('active');
+    eraserBtn.classList.remove('active');
+  });
+  eraserBtn.addEventListener('click', () => {
+    eraserBtn.classList.add('active');
+    colorBtn.classList.remove('active');
+    rainbowBtn.classList.remove('active');
+  });
+};
+
+const makeGrid = function (gridSize) {};
+
+const main = function () {
+  btnClickedAffect();
+};
+main();
