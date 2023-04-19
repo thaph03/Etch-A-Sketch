978ca6088df155f0a5aa1e5491747d4c8978c1f5
Thai Pham <arknightgamersia03@gmail.com>
2023-04-18 19:47:59 -0700

    done with color

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
 'use strict';
 const colorBtn = document.getElementById('colorBtn');
 const rainbowBtn = document.getElementById('rainbowBtn');
-
 const eraserBtn = document.getElementById('eraserBtn');
-
 const clearBtn = document.getElementById('clearBtn');
+const grid = document.querySelector('.grid');
+const colorPicker = document.getElementById('colorPicker');
+const sizeSlider = document.getElementById('sizeSlider');
+const sizeValue = document.getElementById('sizeValue');
 
+//button affects function
 const btnClickedAffect = function () {
+  colorBtn.classList.add('active');
   colorBtn.addEventListener('click', () => {
     colorBtn.classList.add('active');
     rainbowBtn.classList.remove('active');
@@ -25,22 +29,45 @@ const btnClickedAffect = function () {
 };
 
 //function to make grids
-function generateSquares(numSquares) {
-  const grid = document.querySelector('.grid');
-
+const makeGrid = function (numSquares) {
   grid.innerHTML = '';
   grid.style.gridTemplateColumns = `repeat(${numSquares}, 1fr)`;
   grid.style.gridTemplateRows = `repeat(${numSquares}, 1fr)`;
   for (let i = 0; i < numSquares * numSquares; i++) {
     const square = document.createElement('div');
     square.classList.add('square');
+    //drag and change color
+
+    if (colorBtn.classList.contains('active')) {
+      square.addEventListener('mousemove', () => {
+        square.style.backgroundColor = colorPicker.value;
+      });
+    } else {
+      square.addEventListener('mousemove', () => {
+        square.style.backgroundColor = 'blue';
+      });
+    }
+
+    clearBtn.addEventListener('click', () => {
+      square.style.backgroundColor = 'whitesmoke';
+    });
+
     grid.appendChild(square);
   }
-}
+};
 
-generateSquares(2);
+//adjust value along with the slider
+
+const adjustSize = function () {
+  makeGrid(sizeSlider.value);
+  sizeSlider.addEventListener('input', () => {
+    makeGrid(sizeSlider.value);
+    sizeValue.textContent = `${sizeSlider.value} x ${sizeSlider.value}`;
+  });
+};
 
 const main = function () {
   btnClickedAffect();
+  adjustSize();
 };
 main();
